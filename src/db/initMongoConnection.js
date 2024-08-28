8f98248465fd87c1ffe63e4fa0a6195c0d56a83a
Exxin <exxinblood@gmail.com>
2024-08-28 20:44:17 +0300

    main

--- src/db/initMongoConnection.js
+++ src/db/initMongoConnection.js
@@ -1,19 +1,22 @@
+// src/db/initMongoConnection.js
+
 import mongoose from 'mongoose';
 
-const MONGODB_USER = 'exxinblood';
-const MONGODB_PASSWORD = 'o4BsI9P9AjbFMDt9';
-const MONGODB_URL = 'cluster0.8cyzrat.mongodb.net';
-const MONGODB_DB = 'contacts';
-
+import { env } from '../utils/env.js';
 
 export const initMongoConnection = async () => {
   try {
+    const user = env('MONGODB_USER');
+    const pwd = env('MONGODB_PASSWORD');
+    const url = env('MONGODB_URL');
+    const db = env('MONGODB_DB');
+
     await mongoose.connect(
-      `mongodb+srv://${MONGODB_USER}:${MONGODB_PASSWORD}@${MONGODB_URL}/${MONGODB_DB}?retryWrites=true&w=majority&appName=Cluster0`,
+      `mongodb+srv://${user}:${pwd}@${url}/${db}?retryWrites=true&w=majority`,
     );
     console.log('Mongo connection successfully established!');
-  } catch (error) {
-    console.error('Error connecting to MongoDB:', error.message);
+  } catch (e) {
+    console.log('Error while setting up mongo connection', e);
+    throw e;
   }
 };
-
