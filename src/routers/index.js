20f9a3dc5aba8daee23621cf13f3b9c856706472
Exxin <exxinblood@gmail.com>
2024-08-29 12:57:38 +0300

    Update index.js

--- src/routers/index.js
+++ src/routers/index.js
@@ -2,14 +2,14 @@
 
 import { Router } from 'express';
 
-import waterRouter from './water.js';
-import usersRouter from './users.js';
-import authRouter from './auth.js';
+// import waterRouter from './water.js';
+// import usersRouter from './users.js';
+// import authRouter from './auth.js';
 
 const router = Router();
 
-router.use('/auth', authRouter);
-router.use('/users', usersRouter);
-router.use('/water', waterRouter);
+// router.use('/auth', authRouter);
+// router.use('/users', usersRouter);
+// router.use('/water', waterRouter);
 
 export default router;
