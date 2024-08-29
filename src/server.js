c87158b5f8d9d2734c4540cf8923096ed3a8d387
Exxin <exxinblood@gmail.com>
2024-08-29 12:45:05 +0300

    fix server

--- src/server.js
+++ src/server.js
@@ -9,7 +9,7 @@ import { env } from './utils/env.js';
 // import router from './routers/index.js';
 // import { errorHandler } from './middlewares/errorHandler.js';
 // import { notFoundHandler } from './middlewares/notFoundHandler.js';
-import { UPLOAD_DIR } from './constants/index.js';
+// import { UPLOAD_DIR } from './constants/index.js';
 // import { swaggerDocs } from './middlewares/swaggerDocs.js';
 
 const PORT = Number(env('PORT', '3000'));
