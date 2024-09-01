10f190fddc509be88ed374b15d456dcd3e32c108
MariiaHelevera <143526761+MariiaHelevera@users.noreply.github.com>
2024-09-01 18:49:07 +0200

    swagger for water endpoints

--- src/server.js
+++ src/server.js
@@ -5,25 +5,27 @@ import dotenv from "dotenv";
 import env from "./utils/env.js";
 import notFoundHandler from "./middleware/notFoundHandler.js";
 import errorHandler from "./middleware/errorHandler.js";
-
+import { swaggerDocs } from "./middleware/swaggerDocs.js";
 
 
 export default function setupServer() {
-    dotenv.config()
-    const port = env("PORT","3000")
+  dotenv.config()
+  const port = env("PORT","3000")
 
-    const app = express()
-    app.use(express.json())
-     app.use(cors());
-    app.use(
-  pino({
-    transport: {
-      target: 'pino-pretty',
-    },
-  }),
-    );
+  const app = express()
+  app.use(express.json())
+  app.use(cors());
+  app.use(
+    pino({
+      transport: {
+        target: 'pino-pretty',
+      },
+    }),
+  );
 
-    app.use(notFoundHandler)
-app.use(errorHandler)
-    app.listen(port, () => console.log(`Server is running on port ${port} `))
+  app.use(notFoundHandler)
+  app.use(errorHandler)
+  app.use('/uploads', express.static(UPLOAD_DIR));
+  app.use('/api-docs', swaggerDocs());
+  app.listen(port, () => console.log(`Server is running on port ${port} `))
 }
