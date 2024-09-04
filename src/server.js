2aa23567ba21d34bbf14b81681f0cd8257f5dd8b
MariiaHelevera <143526761+MariiaHelevera@users.noreply.github.com>
2024-09-04 20:27:43 +0200

    swagger docs for water, auth, user

--- src/server.js
+++ src/server.js
@@ -6,24 +6,28 @@ import env from "./utils/env.js";
 import notFoundHandler from "./middleware/notFoundHandler.js";
 import errorHandler from "./middleware/errorHandler.js";
 
+import { swaggerDocs } from "./middleware/swaggerDocs.js";
+import swaggerUI from "swagger-ui-express";
 
 
 export default function setupServer() {
-    dotenv.config()
-    const port = env("PORT","3000")
+  dotenv.config()
+  const port = env("PORT","3000")
 
-    const app = express()
-    app.use(express.json())
-     app.use(cors());
-    app.use(
-  pino({
-    transport: {
-      target: 'pino-pretty',
-    },
-  }),
-    );
+  const app = express()
+  app.use(express.json())
+    app.use(cors());
+  app.use(
+    pino({
+      transport: {
+        target: 'pino-pretty',
+      },
+    }),
+  );
 
-    app.use(notFoundHandler)
-app.use(errorHandler)
-    app.listen(port, () => console.log(`Server is running on port ${port} `))
+  app.use("/api-docs", swaggerUI.serve, swaggerDocs());
+
+  app.use(notFoundHandler)
+  app.use(errorHandler)
+  app.listen(port, () => console.log(`Server is running on port ${port} `))
 }
