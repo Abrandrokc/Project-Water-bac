9cdd4b2517ec63b7cf353c02ddcf25165d45e137
Abrandrokc <Abrandrokco@gmail.com>
2024-09-07 18:57:31 +0300

    fix

--- src/services/water.js
+++ src/services/water.js
@@ -36,9 +36,11 @@ export const getWaterPerMonth = async (firstDate, secondDate, userId) => {
   const startDate = new Date(firstDate);
   const endDate = new Date(secondDate);
 
+  
   const user = await UsersCollection.findById(userId);
   const dailyNorma = user.waterAmount;
 
+  
   const results = await Water.find({
     createdAt: {
       $gte: startDate.toISOString(),
@@ -49,31 +51,43 @@ export const getWaterPerMonth = async (firstDate, secondDate, userId) => {
 
   const daysMap = {};
 
+  
   results.forEach(entry => {
     const dateObject = new Date(entry.createdAt);
-    const day = dateObject.getDate();
-    const month = dateObject.toLocaleString('default', { month: 'long' });
     const formattedDate = formatDate(dateObject);
+
+    if (!daysMap[formattedDate]) {
+      daysMap[formattedDate] = {
+        date: formattedDate,
+        dailyTotal: 0,
+        consumptionCount: 0,
+        dailyNorm: dailyNorma,
+      };
+    }
+
+   
     daysMap[formattedDate].dailyTotal += entry.waterVolume;
     daysMap[formattedDate].consumptionCount += 1;
   });
 
+
   const dailyInfo = Object.values(daysMap).map(dayInfo => {
-    let waterPercent = ((dayInfo.dailyTotal / (dayInfo.dailyNorm * 1000)) * 100);
+    const waterPercent = ((dayInfo.dailyTotal / (dayInfo.dailyNorm * 1000)) * 100);
     return {
       ...dayInfo,
       waterPercent: waterPercent.toFixed(2) + '%',
     };
   });
 
+  
   const dates = [];
   const currentDate = new Date(startDate);
-
   while (currentDate <= endDate) {
     dates.push(formatDate(currentDate));
     currentDate.setDate(currentDate.getDate() + 1);
   }
 
+  
   const existingDataMap = new Map(dailyInfo.map(item => [item.date, item]));
   const result = dates.map(date => {
     const entry = existingDataMap.get(date);
@@ -84,7 +98,7 @@ export const getWaterPerMonth = async (firstDate, secondDate, userId) => {
       date: date,
       dailyTotal: 0,
       consumptionCount: 0,
-      dailyNorm: 0,
+      dailyNorm: dailyNorma,
       waterPercent: "0%",
     };
   });
