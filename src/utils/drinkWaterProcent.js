299294c62c6da78b6f2b4dd0eb01e96b0bba4978
Abrandrokc <Abrandrokco@gmail.com>
2024-09-08 21:22:54 +0300

    fix

--- src/utils/drinkWaterProcent.js
+++ src/utils/drinkWaterProcent.js
@@ -1,13 +1,12 @@
 export const drinkWaterProcent = (results, dailyNorm = 1500) => {
-    console.log("Results:", results); // Перевіряємо, що передаємо
+    console.log("Results:", results); 
     const totalAmount = results.reduce((sum, item) => sum + item.waterVolume, 0);
-    console.log("Total Amount of water:", totalAmount); // Лог для загальної кількості води
+    console.log("Total Amount of water:", totalAmount); 
     
-    console.log("Daily Norm:", dailyNorm ); // Лог для перевірки норми
+    console.log("Daily Norm:", dailyNorm ); 
 
     let Procent = parseFloat((totalAmount / (dailyNorm * 1000)* 100).toFixed(2));
-    console.log("Calculated Procent:", Procent); // Лог для перевірки відсотка
-    
+    console.log("Calculated Procent:", Procent);
     if (Procent > 100) {
         return 100;
     }
