6528e2f8c191e96be3f6133871cd4b566c05e99d
Abrandrokc <Abrandrokco@gmail.com>
2024-09-08 20:06:09 +0300

    fix avatar

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,12 +1,14 @@
 import path from "node:path";
-
+import fs from 'fs';
 export const ACCESS_TOKEN_TTL = 15 * 60 * 1000;
 export const REFRESH_TOKEN_TTL = 30 * 24 * 60 * 60 * 1000;
 
-export const TEMP_UPLOAD_DIR = path.resolve("temp");
+export const TEMP_UPLOAD_DIR = path.resolve("srs","temp");
 
-export const UPLOAD_DIR = path.resolve("uploads");
-
+export const UPLOAD_DIR = path.resolve("srs","uploads");
+if (!fs.existsSync(TEMP_UPLOAD_DIR)) {
+  fs.mkdirSync(TEMP_UPLOAD_DIR, { recursive: true });
+}
 export const CLOUDINARY = {
   CLOUD_NAME: "CLOUDINARY_CLOUD_NAME",
   API_KEY: "CLOUDINARY_API_KEY",
